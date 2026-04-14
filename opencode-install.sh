95790186264852af77f73f0db501555cdeb4fed1
9 <9@loli.world>
2026-04-14 12:16:26 +0800

    chore: 更新 opencode-install.sh 安装脚本

--- opencode-install.sh
+++ opencode-install.sh
@@ -3,7 +3,6 @@
 rm -rf ~/.config/opencode/skills/security-audit
 [ -d ~/.config/opencode/skills/security-audit ] && mkdir ~/.config/opencode/skills/security-audit
 rsync -av --delete SKILL.md ~/.config/opencode/skills/security-audit/
-rsync -av --delete scripts ~/.config/opencode/skills/security-audit/
 rsync -av --delete references ~/.config/opencode/skills/security-audit/
 
 find ~/.config/opencode/skills/security-audit -type d -exec chmod 755 {} \;
