5072f0e3af40c9602056ed7a54af435806e6ba9f
9 <9@loli.world>
2026-04-17 15:12:54 +0800

    docs(skill): update rule count and categories; fix open-redirect-javascript false positives

--- test-samples/open_redirect_java.java
+++ test-samples/open_redirect_java.java
@@ -3,15 +3,15 @@ import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.servlet.ModelAndView;
 
 public class OpenRedirectJava {
-    public void vulnerable(HttpServletResponse response, String userUrl) throws Exception {
-        response.sendRedirect(userUrl);
+    public void vulnerable(HttpServletResponse response, String user_url) throws Exception {
+        response.sendRedirect(user_url);
     }
 
-    public RedirectView vulnerableView(String userUrl) {
-        return new RedirectView(userUrl);
+    public RedirectView vulnerableView(String user_url) {
+        return new RedirectView(user_url);
     }
 
-    public ModelAndView vulnerableModel(String userUrl) {
-        return new ModelAndView("redirect:" + userUrl);
+    public ModelAndView vulnerableModel(String user_url) {
+        return new ModelAndView("redirect:" + user_url);
     }
 }
